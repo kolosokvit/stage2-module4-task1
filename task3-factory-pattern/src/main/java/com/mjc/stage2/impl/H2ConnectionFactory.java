f463bb7805b017ffe2b675159fcb11a2b4c64daf
Vitali Kalaskou <kolosokvit@gmail.com>
2024-05-20 14:17:43 +0300

    Builder and Factory patterns

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,27 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        try(FileReader fileReader = new FileReader("h2database.properties")) {
+           Properties properties = new Properties();
+           properties.load(fileReader);
+           String driver = properties.getProperty("jdbc_driver");
+           String url = properties.getProperty("db_url");
+           String user = properties.getProperty("user");
+           String password = properties.getProperty("password");
+           return DriverManager.getConnection(url, user, password);
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
